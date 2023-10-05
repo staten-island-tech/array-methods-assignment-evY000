19d0cb7d9972838332168171b8f44532c6affb49
evY000 <000evan001@gmail.com>
2023-10-05 13:13:43 -0400

    e3hurfjgi0br532om4egftijbm

---
+++ index.js
@@ -0,0 +1,3 @@
+const PlayerOne =[{FirstName: 'Stephen', LastName: 'Curry', position: 'PG', PPG:'29.4', APG:'6.3', RBPG:'6.1'}]
+const PlayerTwo =[{FirstName: 'Devin', LastName: 'Booker', position: 'SG', PPG:'27.8', APG:'5.5', RBPG:'4.5'}]
+const PlayerThree =[{FirstName: 'Devin', LastName: 'Booker', position: 'SG', PPG:'27.8', APG:'5.5', RBPG:'4.5'}]
