9d2f7c176552fead4564057f0877158c35154980
evY000 <000evan001@gmail.com>
2023-10-06 13:13:36 -0400

    000000000000

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const PlayerOne =[{FirstName: 'Stephen', LastName: 'Curry', position: 'PG', PPG:'29.4', APG:'6.3', RBPG:'6.1'}]
-const PlayerTwo =[{FirstName: 'Devin', LastName: 'Booker', position: 'SG', PPG:'27.8', APG:'5.5', RBPG:'4.5'}]
-const PlayerThree =[{FirstName: 'Lebron', LastName: 'James', position: 'SF', PPG:'28.9', APG:'6.8', RBPG:'8.3'}]
-const PlayerFour =[{FirstName: 'Giannis', LastName: 'Antetokonumpo', position: 'PF', PPG:'31.1', APG:'5.7', RBPG:'11.8'}]
-const PlayerFive =[{FirstName: 'Nikola', LastName: 'Jokic', position: 'C', PPG:'24.5', APG:'9.8', RBPG:'11.8'}]
+const Players =[{
+FirstName: 'Stephen', 
+LastName: 'Curry',
+ position: 'PG',
+PPG:'29.4', 
+APG:'6.3',
+RBPG:'6.1'},
+{FirstName: 'Devin',
+LastName: 'Booker', position: 'SG',
+PPG:'27.8', 
+APG:'5.5',
+RBPG:'4.5'},
+{FirstName: 'Lebron', 
+LastName: 'James', 
+position: 'SF', 
+PPG:'28.9',
+APG:'6.8', 
+RBPG:'8.3'},
+{FirstName: 'Giannis', 
+LastName: 'Antetokonumpo',
+ position: 'PF', 
+ PPG:'31.1', 
+ APG:'5.7',
+ RBPG:'11.8'},
+{FirstName: 'Nikola',
+LastName: 'Jokic',
+position: 'C', 
+PPG:'24.5',
+APG:'9.8',
+RBPG:'11.8'}]
+Players.forEach((PPG) => console.log(PPG));
