9bcded9465b220e6ebab1547452a623c479ff71e
evY000 <000evan001@gmail.com>
2023-10-13 13:16:11 -0400

    4eurfghv5utr7tu87rhg4ti5jh

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ LastName: 'Curry',
  position: 'PG',
 PPG:'29.4', 
 APG:'6.3',
-RBPG:'6.1'},
+RBPG:'6.1',
+Stuff:["50", "20", "30"]},
 
 {FirstName: 'Devin',
 LastName: 'Booker', position: 'SG',
 PPG:'27.8', 
 APG:'5.5',
-RBPG:'4.5'},
+RBPG:'4.5',
+Stuff:["10", "40", "60"]},
 
 {FirstName: 'Lebron', 
 LastName: 'James', 
@@ -32,6 +34,7 @@ LastName: 'Jokic',
 position: 'C', 
 PPG:'24.5',
 APG:'9.8',
-RBPG:'11.8'}]
+RBPG:'11.8'}];
 
 Players.forEach((player) => console.log(player.LastName));
+Players.forEach((player) => console.log(player.Players));
